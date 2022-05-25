7dcb44e286f9aa55bbe4a0dcdba235777da086c2
Gennady Badinov <gennady.badinov@gmail.com>
2022-05-25 13:41:14 +0300

    Replace popular posts sorted with `order_by`

--- blog/views.py
+++ blog/views.py
@@ -33,8 +33,8 @@ def serialize_tag(tag):
 
 
 def index(request):
-    posts = Post.objects.annotate(likes_count=Count("likes"))
-    most_popular_posts = sorted(posts, key=get_post_likes_count)[-5:]
+    posts = Post.objects.annotate(likes_count=Count("likes")).order_by("-likes_count")
+    most_popular_posts = posts[:5]
 
     fresh_posts = Post.objects.order_by("published_at")
     most_fresh_posts = list(fresh_posts)[-5:]
