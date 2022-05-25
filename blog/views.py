5c2c60f9d04cee54b15f265869b18a9cfe2040fd
Gennady Badinov <gennady.badinov@gmail.com>
2022-05-25 13:52:03 +0300

    Fix post comments counting queries

--- blog/views.py
+++ blog/views.py
@@ -30,7 +30,7 @@ def serialize_post_optimized(post):
         "title": post.title,
         "teaser_text": post.text[:200],
         "author": post.author.username,
-        "comments_amount": Count(post.comments),
+        "comments_amount": post.comments.count(),
         "image_url": post.image.url if post.image else None,
         "published_at": post.published_at,
         "slug": post.slug,
